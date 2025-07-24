1c2a785bc44775e6546c510e5ba2a028ff58aee3
JagatheshwaranN <jagatheshwaran.n29@gmail.com>
2025-07-23 21:15:54 -0500

    Employee Portal Frontend Development Started

---
+++ employee_portal_frontend/employee_portal/src/Hello.jsx
@@ -0,0 +1,5 @@
+function Hello() {
+    return <h1 className="text-center">Hey Buddy! Welcome to Vite JS</h1>
+}
+
+export default Hello
---
+++ employee_portal_frontend/employee_portal/src/component/ListEmployees.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const ListEmployees = () => {
+    const tempData = [
+        {
+  "id": 101,
+  "firstName": "Alice",
+  "lastName": "Johnson",
+  "emailId": "alice.johnson@example.com",
+  "location": "New York",
+  "domain": "Finance",
+  "contact": "123-456-7890"
+}, {
+  "id": 102,
+  "firstName": "Bob",
+  "lastName": "Smith",
+  "emailId": "bob.smith@example.com",
+  "location": "San Francisco",
+  "domain": "Technology",
+  "contact": "987-654-3210"
+}
+
+
+    ]
+  return (
+    <div>ListEmployees</div>
+  )
+}
+
+export default ListEmployees
